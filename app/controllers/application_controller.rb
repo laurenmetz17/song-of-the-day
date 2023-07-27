6e9e8a3a0bc72c9c86dc787fc30c77c2177a23f3
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-27 18:05:34 -0400

    add user routes to controller and before action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+  
+  private
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+
+  def current_user
+    user = User.find_by(id: session[:user_id])
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+
+    skip_before_action :authorize, only: [:create]
+    
+    def index
+        users = User.all 
+        render json: users
+    end
+
+    def show 
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: {error:"unathorized"}, status: :unathorized
+        end
+    end
+
+    def destroy 
+        user = User.find(params[:id])
+        listner.destroy
+        head :no_content
+    end
+    
+    def create
+        user = User.create(user_params)
+        if User.valid?
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :username, :password, :password_confirmation)
+    end
 end
