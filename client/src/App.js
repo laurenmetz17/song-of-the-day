4f22a41d4f290355b440a3707b16c50e2c31d7fc
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-27 16:43:41 -0400

    add basic resource routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //when creating a post the user will first search a song in top of form
+  //then it will return that song and create it in the system if it doesnt already exist
+  //then attatch song id to the post 
   return (
     <div className="App">
       <header className="App-header">
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :users, except: [:show] do
+    resources :posts
+    resources :playlists
+  end
+  resources :songs, except: [:index]
+
+
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
