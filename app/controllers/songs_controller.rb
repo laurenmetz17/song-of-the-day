c6e7a7b2a17c9df7aed5f5d4f19cc086bf79cfc2
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-27 18:12:54 -0400

    add song routes to controller and add has_secure_password to user

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,35 @@
 class SongsController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+    
+    def index
+        songs = Song.all
+        render json: songs
+    end
+
+    def show
+        song = Song.find(params[:id])
+        render json: song
+    end
+
+    def destroy 
+        song = find(params[:id])
+        song.destroy
+        head :no_content
+    end
+
+    def create
+        song = Song.create(song_params)
+        if song.valid?
+            render json: song, status: :created
+        else
+            render json: {errors: song.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def song_params
+        params.permit(:title, :artist)
+    end 
+
 end
