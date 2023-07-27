b75129dcbe48d48bfab7694e96fa08d4039fafd7
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-27 19:55:24 -0400

    add post controller routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+
+    def create
+        post = current_user.posts.create(post_params)
+        if post.valid?
+            ##adds song to playlist here?
+            render json: post, status: :created
+        else
+            render json: {errors: post.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        post = current_user.posts.find_by(id: params[:id])
+        post.update(comment: params[:comment])
+        if post.valid?
+            render json: post
+        else
+            render json: {errors: post.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        post = current_user.posts.find_by(id: params[:id])
+        post.destroy
+        head :no_content
+    end
+
+    private
+
+    def post_params
+        params.permit(:song_id, :user_id, :comment)
+    end
+
 end
