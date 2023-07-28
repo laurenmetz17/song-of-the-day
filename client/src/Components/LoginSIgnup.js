de92bc4a481450029727ea87149edb3574ffb6b2
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-28 11:22:42 -0400

    add login form

--- client/src/Components/LoginSIgnup.js
+++ client/src/Components/LoginSIgnup.js
@@ -1,10 +1,35 @@
-import {React, useState} from 'react';
+import {React, useState, useContext} from 'react';
 
 function LoginSignup() {
 
+    const [loginForm, setLoginForm] = useState({
+        username: "",
+        password: ""
+    })
+
+
+    function updateLogin(e) {
+
+    }
+
+    function handleLogin(e) {
+
+    }
 
     return (
-        <p>login</p>
+
+        <form id="login" className="forms" onSubmit={handleLogin}>
+            <h1 className="headers">Login Here</h1>
+            <div>
+                <label>Userame</label>
+                <input id="username" type="text" onChange={updateLogin}/>
+            </div>
+            <div className='headers'>
+                <label>Password</label>
+                <input id="password" type="text" onChange={updateLogin}/>
+            </div>
+            <input id="submit" type="submit" value="Submit"/>
+        </form>
     )
 }
 
