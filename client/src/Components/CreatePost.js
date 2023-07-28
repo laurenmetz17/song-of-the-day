560681c003343dabcd7fa95c261804924ca61892
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-28 16:32:36 -0400

    get song search returning from itunes api

--- client/src/Components/CreatePost.js
+++ client/src/Components/CreatePost.js
@@ -12,13 +12,46 @@ function CreatePost() {
     })
 
     function updateSongForm(e) {
+        setSongError(false)
         const target = e.target.name
         setSongForm({...songForm, [target] : e.target.value})
     }
 
+    function searchSong(e) {
+        e.preventDefault()
+        //figure out how to use the spotify api for cooler app
+        fetch(`https://itunes.apple.com/search?media=music&entity=song&term=${songForm.title}`)
+        .then(resp => {
+            if (resp.ok) {
+                resp.json()
+                .then(data => {
+                    const songsMatch = data.results.filter(songItem => songItem.artistName == songForm.artist);
+                    const songData = songsMatch[0]
+                    if (songData) {
+                        const songArt = songData.artworkUrl100
+                        console.log(songData)
+                        console.log(songArt)
+                    }
+                    else {
+                        setSongError(true)
+                    }
+                    //could match song by track id
+
+                })
+            }
+            else {
+                setSongError(true)
+            }
+        })
+        setSongForm({title: "", artist: ""})
+        e.target.children[1].children[1].value = ""
+        e.target.children[2].children[1].value = ""
+
+    }
+
     return (
         <div className='container'>
-            <form className="forms" >
+            <form className="forms" onSubmit={searchSong} >
                 <h1 className="headers">Search Song</h1>
                 <div className="inputs">
                     <label>Title</label>
@@ -30,6 +63,7 @@ function CreatePost() {
                 </div>
                 <input type="submit" value="Search Song"/>
             </form>
+            {songError ? <p style={{color: "red"}}>Invalid Song</p> : null}
         </div>
     )
 }
