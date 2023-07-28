96671f86dcdc728049b20c196ea7ac501c20ddcb
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-28 11:03:11 -0400

    add user context setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 
 import './App.css';
+import {React, useEffect, useState,useContext, createContext} from 'react';
 import NavBar from './Components/NavBar';
-import LoginSignup from './Components/LoginSIgnup';
-
+import LoginSignup from './Components/LoginSignup';
 
 function App() {
+
+  const UserContext = createContext()
+  const [user, setUser] = useState(null)
+
+  
 
   //when creating a post the user will first search a song in top of form
   //then it will return that song and create it in the system if it doesnt already exist
   //then attatch song id to the post 
   return (
     <div className="App">
-      <NavBar/>
-      <LoginSignup/>
+      <UserContext.Provider value={user}>
+        <NavBar/>
+        <LoginSignup/>
+      </UserContext.Provider>
     </div>
   );
 }
