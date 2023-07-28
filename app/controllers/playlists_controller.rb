c27c15aaa94dc40bad8a4a2a5552dedefcabcc34
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-27 20:14:21 -0400

    add playlist controller routes and uniqueness of title validation

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,26 @@
 class PlaylistsController < ApplicationController
+
+    def create
+        playlist = current_user.playlists.create(playlist_params)
+        if playlist.valid?
+            render json: playlist,status: :created
+        else
+            render json: {errors: playlist.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def index
+        playlists = current_user.playlists
+        render json: playlists
+    end
+
+    def show
+        playlist = current_user.playlists.find_by(id: params[:id])
+        if playlist
+            render json: playlist
+        else
+            render json: {error:"Playlist does not exist"}, status: :not_found
+        end
+    end
+
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,8 @@
 class Playlist < ApplicationRecord
     validates :title, {presence:true}
     validates :user_id {presence:true}
-    
+    validates :title, {uniqueness:true}
+
     belongs_to :user
     has_many :posts
     has_many :songs, through: :post
