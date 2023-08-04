d44ad85b683c6ab3dd8b706ac08aefda78f2727b
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-08-04 15:17:57 -0400

    add conditional button to navigate to post form if they havent made a post today

--- client/src/Components/SongOfTheDayCard.js
+++ client/src/Components/SongOfTheDayCard.js
@@ -1,9 +1,11 @@
 import UserContext from "./UserContext";
 import {useContext} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function SongOfTheDayCard() {
 
     const user = useContext(UserContext)
+    const navigate = useNavigate()
     const date = new Date()
     let todayPost
     if (user) {
@@ -20,8 +22,7 @@ function SongOfTheDayCard() {
         <div className="song_of_the_day">
             {user? <h3>Hello {user.name}!</h3> :<h3>Login to make posts</h3>}
             <h4 className="date">{date.toDateString().substring(4,10)}, {date.getFullYear()}</h4>
-            {todayPost ? <p>{todayPost.comment}</p> : null}
-
+            {todayPost.empty ? <p>{todayPost.comment}</p> : <button onClick={() => {navigate('/postToday')}}>Choose Your Song Of The Day</button>}
         </div>
     )
 }
