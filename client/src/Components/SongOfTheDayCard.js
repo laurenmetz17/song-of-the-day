c32acb442f7f1084c0a83c0ffe3e6e221bcfb32c
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-08-08 18:10:02 -0400

    add edit form to the song of the day card

--- client/src/Components/SongOfTheDayCard.js
+++ client/src/Components/SongOfTheDayCard.js
@@ -1,11 +1,12 @@
 import UserContext from "./UserContext";
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 function SongOfTheDayCard() {
 
     const user = useContext(UserContext)
     const navigate = useNavigate()
+    const [showEdit,setShowEdit] = useState(false)
     const date = new Date()
 
     //if the user has as post today get that post and song
@@ -19,6 +20,16 @@ function SongOfTheDayCard() {
         }
     }
 
+    function showForm() {
+
+        return (
+            <form>
+                <input type="text" name="comment_edit"/>
+                <input type="submit"/>
+            </form>
+        )
+    }
+
     function postMade() {
         //if there is a post today render the post, if not render a button to go the create post page
         if (todayPost.length == 0) {
@@ -26,20 +37,24 @@ function SongOfTheDayCard() {
         }
         else {
             return (
-                <div>
+                <div className="post_card">
                     <h1>Today's Song</h1>
                     <img src={todaySong.art} alt="album cover"></img>
                     <p>{todaySong.title}</p>
                     <p>{todaySong.artist}</p>
                     <p>{todayPost.comment}</p>
+                    <button onClick={() => setShowEdit(true)}>Edit Comment</button>
+                    {showEdit ? showForm(): null}
                 </div>
             )
         }
     }
 
+    //add in ability to edit the comment od the post
+
 
     return (
-        <div className="song_of_the_day">
+        <div className="song_of_the_day_card">
             {user? <h3>Hello {user.name}!</h3> :<h3>Login to make posts</h3>}
             <h4 className="date">{date.toDateString().substring(4,10)}, {date.getFullYear()}</h4>
             {user ? postMade() : null}
